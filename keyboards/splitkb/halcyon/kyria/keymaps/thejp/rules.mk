4e8946dac93313841a5edc5a14b06a0625a58297
TheJP <jp_55555@hotmail.com>
2025-07-20 17:23:45 +0200

    Finished initial version of thejp keymap

--- keyboards/splitkb/halcyon/kyria/keymaps/thejp/rules.mk
+++ keyboards/splitkb/halcyon/kyria/keymaps/thejp/rules.mk
@@ -1,4 +1,7 @@
 ENCODER_MAP_ENABLE = yes
 
+# Added so we can use encoders to scroll.
+MOUSEKEY_ENABLE = yes
+
 # This adds module functionality to your keyboard (files found in users/halcyon_modules)
 USER_NAME := halcyon_modules
